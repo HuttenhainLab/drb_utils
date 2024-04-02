71bd06e1362d25471faab1d3e8d246f7109d9a71
Dain Brademan <dainbrademan@gmail.com>
2024-04-02 11:32:52 -0500

    Update data_management.R
    Added Make.LFQ.Contrast.Matrix function

--- data_management.R
+++ data_management.R
@@ -51,8 +51,55 @@ Check.MsStats.DataProcess.Output <- function (input.data.frame, type = ""){
   }
 }
 
+#'  Generates an MsStats contrast matrix for
+#'  Abundance Proteomics MsStats Group Processing
+#' @param input.data.frame The MsStats dataframe to check. Should be in the same shape as the data process function output.
+#' @param condition.vector A character vector containing the condition-condition comparisons in the format "Condition1-Condition2"
+#' @param exact.match Determines if regular expressions or literal direct matches should be used for contrasts
+Make.LFQ.Contrast.Matrix <- function(input.data.frame, condition.vector, exact.match = TRUE){
+  Check.MsStats.DataProcess.Output(input.data.frame)
+  columnNames <- as.character(levels(input.data.frame$ProteinLevelData$GROUP))
+  
+  positives = c()
+  negatives = c()
+  for (i in columnNames){
+    for (j in columnNames){
+      if (i != j){
+        positives <- c(positives,i)
+        negatives <- c(negatives,j)
+      }
+    }
+  }
+  contrasts <- data.table (positives, negatives)
+  contrasts[,name := paste(positives,negatives, sep="-")]
+  
+  contrastNames <- character(0)
+  for (re in condition.vector){
+    if (exact.match) {
+      contrastNames <- union(contrastNames, grep(paste0("^", re, "$"), contrasts$name, value = TRUE))
+    } else {
+      contrastNames <- union(contrastNames, grep(re, contrasts$name, value = TRUE))
+    }
+    
+    
+  }
+  print (contrastNames)
+  contrasts <- contrasts[name %in% contrastNames]
+  print (contrasts)
+  
+  contrastMat <- matrix(rep(0,length(columnNames) * nrow(contrasts)), nrow=nrow(contrasts), ncol=length(columnNames),
+                        dimnames = list(contrasts$name, columnNames))
+  
+  for (i in seq_len(nrow(contrasts))){
+    #rows are named according to the positive condition:
+    contrastMat[contrasts$name[i],contrasts$negatives[i]] = -1
+    contrastMat[contrasts$name[i],contrasts$positives[i]] =  1
+  }
+  return (contrastMat)  
+}
+
 #'  Generates an MsStats contrast matrix for 
-#'  MsStats Group Processing
+#'  Post-translational Modification MsStats Group Processing
 #' @param input.data.frame The MsStats dataframe to check. Should be in the same shape as the data process function output.
 #' @param condition.vector A character vector containing the condition-condition comparisons in the format "Condition1-Condition2"
 #' @param exact.match Determines if regular expressions or literal direct matches should be used for contrasts
